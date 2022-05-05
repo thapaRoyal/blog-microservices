15b688aebc576ce2b62a2d782f713bbf59254afe
thapaRoyal <thaparoyal17@gmail.com>
2022-05-05 14:03:38 +0545

    building post submission

---
+++ client/src/PostCreate.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+export default () => {
+  const [title, setTitle] = useState('');
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    await axios.post('http://localhost:4000/posts', {
+      title,
+    });
+
+    setTitle('');
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Title</label>
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            type="text"
+            className="form-control"
+          />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+};

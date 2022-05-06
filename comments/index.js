1d58c214e15bc622b127682b09840df078d1d736
thapaRoyal <thaparoyal17@gmail.com>
2022-05-06 14:48:20 +0545

    Adding comment moderation

--- comments/index.js
+++ comments/index.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const { randomBytes } = require("crypto");
-const cors = require("cors");
-const axios = require("axios");
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,38 +10,39 @@ app.use(cors());
 
 const commentsByPostId = {};
 
-app.get("/posts/:id/comments", (req, res) => {
+app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", async (req, res) => {
-  const commentId = randomBytes(4).toString("hex");
+app.post('/posts/:id/comments', async (req, res) => {
+  const commentId = randomBytes(4).toString('hex');
   const { content } = req.body;
 
   const comments = commentsByPostId[req.params.id] || [];
 
-  comments.push({ id: commentId, content });
+  comments.push({ id: commentId, content, status: 'pending' });
 
   commentsByPostId[req.params.id] = comments;
 
-  await axios.post("http://localhost:4005/events", {
-    type: "CommentCreated",
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
     data: {
       id: commentId,
       content,
       postId: req.params.id,
+      status: 'pending',
     },
   });
 
   res.status(201).send(comments);
 });
 
-app.post("/events", (req, res) => {
-  console.log("Event Received", req.body.type);
+app.post('/events', (req, res) => {
+  console.log('Event Received', req.body.type);
 
   res.send({});
 });
 
 app.listen(4001, () => {
-  console.log("Listening on 4001");
+  console.log('Listening on 4001');
 });

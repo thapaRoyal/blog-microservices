1ba1b246ce281ed8be238684c400f5e9ba9c40f7
thapaRoyal <thaparoyal17@gmail.com>
2022-05-06 14:57:12 +0545

    Handling moderation

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,25 +1,28 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const axios = require("axios");
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
 
-app.post("/events", (req, res) => {
+app.post('/events', (req, res) => {
   const event = req.body;
 
-  axios.post("http://localhost:4000/events", event).catch((err) => {
+  axios.post('http://localhost:4000/events', event).catch((err) => {
     console.log(err.message);
   });
-  axios.post("http://localhost:4001/events", event).catch((err) => {
+  axios.post('http://localhost:4001/events', event).catch((err) => {
     console.log(err.message);
   });
-  axios.post("http://localhost:4002/events", event).catch((err) => {
+  axios.post('http://localhost:4002/events', event).catch((err) => {
     console.log(err.message);
   });
-  res.send({ status: "OK" });
+  axios.post('http://localhost:4003/events', event).catch((err) => {
+    console.log(err.message);
+  });
+  res.send({ status: 'OK' });
 });
 
 app.listen(4005, () => {
-  console.log("Listening on 4005");
+  console.log('Listening on 4005');
 });
--- moderation/index.js
+++ moderation/index.js
@@ -4,7 +4,23 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/events', (req, res) => {});
+app.post('/events', async (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === 'CommentCreated') {
+    const status = data.content.includes('orange') ? 'rejected' : 'approved';
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentModerated',
+      data: {
+        id: data.id,
+        postId: data.postId,
+        status,
+        content: data.content,
+      },
+    });
+  }
+  res.send({});
+});
 
 app.listen(4003, () => {
   console.log('listening on port 4003');

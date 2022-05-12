2c993a9c73555474d8bbd2d601524c9db5b90b9b
thapaRoyal <thaparoyal17@gmail.com>
2022-05-12 08:22:06 +0545

    Adding query, moderation and comments

--- query/index.js
+++ query/index.js
@@ -50,7 +50,7 @@ app.post('/events', (req, res) => {
 app.listen(4002, async () => {
   console.log('Listening on 4002');
 
-  const res = await axios.get('http://localhost:4005/events');
+  const res = await axios.get('http://event-bus-srv:4005/events');
 
   for (let event of res.data) {
     console.log('Processng event :', event.data);
